aed86b315566d5387909c1a321c338cc28942e33
carlasolrocca <carlasolrocca@gmail.com>
2024-07-17 23:43:21 -0300

    archivo

---
+++ final-natacion.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+
+struct competencia{
+    int nroCompetidor; //mayor a cero y van en orden
+    char apellido[30];
+    char estilo;
+    int distancia;
+    float tiempo;
+    int activo; //1 o 0
+};
+
+
+
+
+int main(){
+
+
+
+    getchar();
+    return 0;
+}
